c3e4c5114064bb46913906d6d4cdf2dc493b5d64
RecodeCBT <recodecbt@gmail.com>
2025-07-29 05:48:56 +0100

    Update App.tsx
    Added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import ChatCBT from "./pages/ChatCBT";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
@@ -65,6 +66,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
