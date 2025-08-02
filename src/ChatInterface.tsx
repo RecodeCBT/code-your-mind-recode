9825a7fdcca692c6272ca6fa58824e99f2e83444
RecodeCBT <recodecbt@gmail.com>
2025-08-02 11:37:20 +0100

    Update ChatInterface.tsx
    Added "s" to recode-chat to make chatbot hopefully function

--- src/ChatInterface.tsx
+++ src/ChatInterface.tsx
@@ -20,7 +20,7 @@ export default function ChatInterface() {
 
   try {
     // API call
-    const response = await fetch('/api/recode-chat', {
+    const response = await fetch('/api/recode-chats', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ message: input }),
